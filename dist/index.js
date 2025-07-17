7d674b432a248c4ab4919e3c49efcfd539ac573a
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2025-07-17 06:06:35 +0600

    change root host

--- dist/index.js
+++ dist/index.js
@@ -17,7 +17,7 @@ app.use(express_1.default.json());
 app.use(express_form_data_1.default.parse());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://booknest-client-zy9d.vercel.app"],
     credentials: true,
 }));
 app.use(body_parser_1.default.json({ limit: "20mb" }));
--- dist/src/utils/baseUrl.js
+++ dist/src/utils/baseUrl.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // baseURL.ts
-const BASE_URL = "http://localhost:5173";
+// const BASE_URL = "http://localhost:5173";
+const BASE_URL = "https://booknest-client-zy9d.vercel.app";
 exports.default = BASE_URL;
